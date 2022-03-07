4ad75ab034eef49881406ca8e6d53945d8e98ab7
lety <lety@email.com>
2022-03-07 09:29:28 -0600

    este es mi tercer commit

--- casita.java
+++ casita.java
@@ -1,4 +1,12 @@
 public class casita{
 public static void main (String []args{
+   System.out.println("    *********  ");
+   System.out.println("  *            *");
+   System.out.println("*                 *");
+   System.out.println("*******************");
+   System.out.println("* *  *** ***** *  *");
+   System.out.println("* *  **    *   *  *");
+   System.out.println("* ** ***   *   *  *");
+   System.out.println("*******************");
 }
 }
